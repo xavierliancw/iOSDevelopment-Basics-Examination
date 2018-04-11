460d223a3d027ae95721f8f20e12feb97187f87e
Xavier Lian <xavierliancw@gmail.com>
2018-04-11 00:52:00 -0700

    Changes
    - Algorithms now announce that they're running through print statements
    - Added a tire tread image
    - Applied tire tread images to VwTire.xib

--- Swiftly Test Your Might/Swiftly Test Your Might/Algorithms.swift
+++ Swiftly Test Your Might/Swiftly Test Your Might/Algorithms.swift
@@ -10,21 +10,25 @@ import Foundation
 
 func algorithm1(_ str: String) -> Character?
 {
+    print("Running algorithm 1 for task kale!")
     return nil
 }
 
 func algorithm2(_ wheels: [Wheel?]) -> [Wheel?]
 {
+    print("Running algorithm 2 for task lettuce!")
     return [Wheel]()
 }
 
 func algorithm3(_ wheels: [Wheel?]) -> [Date]
 {
+    print("Running algorithm 3 for task carrot!")
     return [Date]()
 }
 
 func algorithm4(_ json: Data) -> [Spinach]
 {
+    print("Running algorithm 4 for task spinach!")
     return [Spinach]()
 }
 struct Spinach: Codable
@@ -35,5 +39,6 @@ struct Spinach: Codable
 
 func lastAlgorithm(_ data: Data) -> [Wheel]
 {
+    print("Running the last algorithm!")
     return [Wheel]()
 }
